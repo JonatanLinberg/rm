c8caa98c3205dffc001000be532b5def63040cf2
Jonatan Linberg <98joli@gmail.com>
2017-03-04 21:11:57 +0100

    0.1.0

--- rm.py
+++ rm.py
@@ -2,3 +2,36 @@
 
 from tkinter import *
 
+root = Tk()
+
+#const
+sqside = 50
+xsq = 8
+ysq = 8
+
+wwidth = xsq * sqside
+wheight = ysq* sqside
+
+#declare arrays
+square = [["" for y in range(ysq)] for x in range(xsq)]
+
+#Player
+px = 0
+py = 0
+
+w = Canvas(root, width=wwidth, height=wheight, highlightthickness=0)
+w.pack()
+
+def move(event)
+	
+
+root.bind('<Up>', move)
+root.bind('<Down>', move)
+root.bind('<Left>', move)
+root.bind('<Right>', move)
+
+
+root.focus_set()
+
+init()
+mainloop()
