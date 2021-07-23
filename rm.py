dfb6ffd90ac73c84cb5ca38e97a935e6987cb23b
Jonatan Linberg <98joli@gmail.com>
2021-07-23 11:32:29 +0200

    Added points-label

--- rm.py
+++ rm.py
@@ -1,120 +1,149 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python2.7
 
-from tkinter import *
-from tkinter import messagebox
+from Tkinter import *
 from random import randint
+import tkMessageBox
+from os import system
+
+pos = 3
+count = 1
+cakeArray = [False for c in range(0, 7)]
+points = 0
+cakecount = 0
+running = True
 
 root = Tk()
+root.resizable(width=False, height=False)
+root.geometry("400x300")
 
-#const
-sqside = 50
-xsq = 9
-ysq = 9
-loadedchunks = 9
+img = PhotoImage(file = "./rsrc/rasmus.gif")
+cake = PhotoImage(file = "./rsrc/cake.gif")
+bg = PhotoImage(file = "./rsrc/bg.gif")
 
-wwidth = xsq * sqside
-wheight = ysq* sqside
+bgl = Label(image=bg, width = 400, height = 300)
+bgl.image = bg # keep a reference!
+bgl.pack()
 
-#declare arrays
-#'black' = wall
-#'white' = open
-#'gray' = cake
-chunk = [[["" for y in range(ysq)] for x in range(xsq)] for c in range(loadedchunks)]
+label = Label(image=img, width = 40, height = 40)
+label.image = img # keep a reference!
 
-#'' = no wall
-#'u' = wall up
-#'l' = wall left
-#'r' = wall right
-#'d' = wall down
-withwall = [[['' for y in range(ysq)] for x in range(xsq)] for c in range(loadedchunks)]
+cl0 = Label(image=cake, width = 40, height = 40)
+cl0.image = cake
 
-#Player
-px = 4
-py = 4
+cl1 = Label(image=cake, width = 40, height = 40)
+cl1.image = cake
 
-w = Canvas(root, width=wwidth, height=wheight, highlightthickness=0)
-w.pack()
+cl2 = Label(image=cake, width = 40, height = 40)
+cl2.image = cake
 
-def paint():
-	#ground
-	for x in range(xsq):
-		for y in range(ysq):
-			w.create_rectangle(x*sqside, y*sqside, (x+1)*sqside, (y+1)*sqside, \
-				fill = chunk[5][x][y], outline = 'black')
-	w.create_oval(px*sqside, py*sqside, (px+1)*sqside, (py+1)*sqside, fill='blue')
+cl3 = Label(image=cake, width = 40, height = 40)
+cl3.image = cake
 
-def move(event):
-	print ("struggling to move...")
+cl4 = Label(image=cake, width = 40, height = 40)
+cl4.image = cake
 
-def genWorld(c):
-		for x in range(xsq):
-			for y in range(ysq):
-				#print('trying square ' + str(c) + str(x) + str(y))
-				p=6
-				if x > 0 and y > 0 and x < xsq-1 and y < ysq-1:
-					if chunk[c][x-1][y] == 'black':
-						p -= 2
-						withwall[c][x-1][y] += 'r'
-						if withwall[c][x-1][y] == 'l':
-							p -=5
-						if withwall[c][x-1][y] == 'd' or withwall[c][x-1][y] == 'u':
-							p +=20
-						print ('left ' + withwall[c][x-1][y])
+cl5 = Label(image=cake, width = 40, height = 40)
+cl5.image = cake
 
-					if chunk[c][x+1][y] == 'black':
-						p -= 2
-						withwall[c][x+1][y] += 'l'
-						if withwall[c][x+1][y] == 'r':
-							p -= 5
-						if withwall[c][x+1][y] == 'd' or withwall[c][x+1][y] == 'u':
-							p += 20
-						print ('right ' + withwall[c][x+1][y])
+cl6 = Label(image=cake, width = 40, height = 40)
+cl6.image = cake
 
-					if chunk[c][x][y-1] == 'black':
-						p -= 2
-						withwall[c][x][y-1] += 'd'
-						if withwall[c][x][y-1] == 'u':
-							p -= 5
-						if withwall[c][x][y-1] == 'r' or withwall[c][x][y-1] == 'l':
-							p += 20
-						print ('up ' + withwall[c][x][y-1])
-
-					if chunk[c][x][y+1] == 'black':
-						p -= 2
-						withwall[c][x][y+1] += 'u'
-						if withwall[c][x][y+1] == 'd':
-							p -= 5
-						if withwall[c][x][y+1] == 'r' or withwall[c][x][y+1] == 'l':
-							p += 20
-						print ('down ' + withwall[c][x][y+1])
-
-				if p <= 0 or randint(0, p) == 0:
-					chunk[c][x][y] = 'black'
-				else:
-					chunk[c][x][y] = 'white'
-					if randint(0, 100)==0:
-						chunk[c][x][y] = 'gray'
-				print('Chunk ' + str(c) + '\n' + str(x) + str(y) +' is ' + chunk[c][x][y] + '\np =' + str(p) + '\n\n')
-		chunk[5][4][4] = 'white'
-
-def init():
-	for c in range(loadedchunks):
-		genWorld(c)
-	paint()
-
-def pause(event):
-	if (messagebox.askquestion('Please don\'t go','Are you sure you want to exit?')=='yes'):
-		sys.exit()
-
-	
-root.bind('<Escape>', pause)
-root.bind('<Up>', move)
-root.bind('<Down>', move)
-root.bind('<Left>', move)
-root.bind('<Right>', move)
+pts_str = StringVar()
+pts_label = Label(textvariable=pts_str)
+pts_label.place(x=0, y=0)
 
 
+def update_pts_label():
+	global points
+	pts_str.set("You have eaten %d cakes."%(points))
+
+def Left(e):
+	global pos
+	if pos > 0:
+		pos -= 1
+
+def Right(e):
+	global pos
+	if pos < 6:
+		pos += 1
+
+def eat(e):
+	global pos, points, cakeArray, cakecount
+	if cakeArray[pos] == False:
+		gameOver()
+	else:
+		cakeArray[pos] = False
+		points += 1
+	if pos == 0:
+		cl0.place_forget()
+	if pos == 1:
+		cl1.place_forget()
+	if pos == 2:
+		cl2.place_forget()
+	if pos == 3:
+		cl3.place_forget()
+	if pos == 4:
+		cl4.place_forget()
+	if pos == 5:
+		cl5.place_forget()
+	if pos == 6:
+		cl6.place_forget()
+	cakecount -= 1
+
+def quit(e):
+	gameOver()
+
+def gameOver():
+	global running
+	running = False
+	tkMessageBox.showinfo("Game Over", "You ate " + str(points) + " cakes.")
+	root.quit()
+
+def loop():
+	global label, count, cakeArray, points, cakecount, running
+	if running:
+		if cakecount == 7:
+			gameOver()
+		count -= 1
+		if count <= 0:
+			c = 60 - points * 3
+			if c < 20:
+				c = 20
+			count = c
+			a = True
+			while (a):
+				r = randint(0, 6)
+				if cakeArray[r] == False:
+					a = False
+
+			if r == 0:
+				cl0.place(x = 60, y = 100)
+			if r == 1:
+				cl1.place(x = 100, y = 100)
+			if r == 2:
+				cl2.place(x = 140, y = 100)
+			if r == 3:
+				cl3.place(x = 180, y = 100)
+			if r == 4:
+				cl4.place(x = 220, y = 100)
+			if r == 5:
+				cl5.place(x = 260, y = 100)
+			if r == 6:
+				cl6.place(x = 300, y = 100)
+			cakecount += 1
+			cakeArray[r] = True	
+
+		label.place(x = 60 + (pos * 40), y = 200)
+		update_pts_label()
+
+		root.after(20, loop)
+
+root.bind('<Left>', Left)
+root.bind('<Right>', Right)
+root.bind('<space>', eat)
+root.bind('<Escape>', quit)
+
+system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
 root.focus_set()
-
-init()
+root.after(100, loop)
 mainloop()
